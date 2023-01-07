fcfbbb3b26c53ba261ee1565825553f901a2ed15
EC2 Default User <ec2-user@ip-172-31-33-21.ap-northeast-1.compute.internal>
2023-01-07 13:58:04 +0000

    Task4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,42 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :items,         only: [:new, :create, :index, :show, :edit, :update]
+    resources :genres,        only: [:index, :create, :edit, :update]
+    resources :customers,     only: [:index, :show, :edit, :update]
+    resources :orders,        only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+
+  scope module: :public do
+    root to: "homes#top"
+
+    resources :items,      only: [:index, :show]
+
+    resources :customers,  only: [:show, :edit, :update]
+    get    'unsubscribe' => 'customers#unsubscribe'
+    patch  'withdrawal'  => 'customers#withdrawal'
+
+    resources :cart_items, only: [:create, :index, :update, :destroy]
+    delete 'destroy_all' => 'cart_items#destroy_all'
+
+    resources :orders,     only: [:new, :create, :index, :show]
+    post   'confirm'     => 'orders#confirm'
+    get    'complete'    => 'orders#complete'
+
+    resources :addresses,  only: [:create, :index, :edit, :update, :destroy]
+
+    get "/about" => "homes#about", as: "about"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
