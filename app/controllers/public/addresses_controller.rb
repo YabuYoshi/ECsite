4a39c78c214fd8eadf5b1bd40a487f707050899d
EC2 Default User <ec2-user@ip-172-31-33-21.ap-northeast-1.compute.internal>
2023-01-08 13:31:25 +0000

    Task4

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,62 @@
 class Public::AddressesController < ApplicationController
+  def index
+    @address = Address.new(address_params)
+    @addresses = Address.all
+
+    respond_to do |wants|
+      wants.html # index.html.erb
+      wants.xml  { render :xml => @addresses }
+    end
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def create
+    @address = Address.new(params[:address])
+
+    respond_to do |wants|
+      if @address.save
+        flash[:notice] = 'Address was successfully created.'
+        wants.html { redirect_to "/address" }
+        wants.xml  { render :xml => @address, :status => :created, :location => @address }
+      else
+        wants.html { render :action => "new" }
+        wants.xml  { render :xml => @address.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @address = Address.find(params[:id])
+
+    respond_to do |wants|
+      if @address.update_attributes(params[:address])
+        flash[:notice] = 'Address was successfully updated.'
+        wants.html { redirect_to(@address) }
+        wants.xml  { head :ok }
+      else
+        wants.html { render :action => "edit" }
+        wants.xml  { render :xml => @address.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+
+    respond_to do |wants|
+      wants.html { redirect_to(addresss_url) }
+      wants.xml  { head :ok }
+    end
+  end
+
+  private
+
+  def address_params
+   params.require(:address).permit(:name, :postal_code, :address)
+  end
+
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,40 @@
 class Public::CustomersController < ApplicationController
+
+ def show
+  @customer = Customer.find(params[:id])
+  respond_to do |wants|
+    wants.html # show.html.erb
+    wants.xml  { render :xml => @customer }
+  end
+ end
+
+ def edit
+   @customer = Customer.find(params[:id])
+ end
+
+ def update
+   @customer = Customer.find(params[:id])
+
+     if @customer.update(customer_params)
+       flash[:notice] = 'Customer was successfully updated.'
+       redirect_to customer_path(@customer.id)
+     else
+       render :edit
+     end
+ end
+
+ def unsubscribe
+ end
+
+ def withdrawal
+ end
+
+ private
+
+ def customer_params
+   params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+ end
+
 end
+
+
