6ab9d3c6fd49e3baaa2b31f1ef6172f1e7cf8de1
EC2 Default User <ec2-user@ip-172-31-33-21.ap-northeast-1.compute.internal>
2023-01-16 14:04:04 +0000

    Task4

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,31 @@
 class Admin::CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.page(params[:page])
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+      if @customer.update(customer_params)
+        flash[:notice] = 'ModelClassName was successfully updated.'
+        redirect_to
+      else
+        render :index
+      end
+  end
+
+  protected
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+  end
+
 end
