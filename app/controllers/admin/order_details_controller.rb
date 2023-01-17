c0e35e4abdadd3e5ad37f7ec71786806d3fe89d5
EC2 Default User <ec2-user@ip-172-31-33-21.ap-northeast-1.compute.internal>
2023-01-17 13:24:02 +0000

    Task4

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,17 @@
 class Admin::OrderDetailsController < ApplicationController
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+      if @order_detail.update(order_detail_params)
+        flash[:notice] = 'ModelClassName was successfully updated.'
+        redirect_to
+      else
+        render :action
+      end
+  end
+
+  private
+  def order_detail_params
+    params.require(:order_detail).permit(:item_id, :amount, :price, :making_status)
+  end
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,21 @@
 class Admin::OrdersController < ApplicationController
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+      if @order.update(order_params)
+        flash[:notice] = 'ModelClassName was successfully updated.'
+        redirect_to
+      else
+        render :action
+      end
+  end
+
+  protected
+  def order_params
+    params.require(:order).permit(:shipping_cost, :postal_code, :address, :name, :total_payment, :order_status, :payment_method)
+  end
 end
+
