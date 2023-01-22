d30de5a0eeb6e402a941be8a695f1e492c8d5d77
EC2 Default User <ec2-user@ip-172-31-33-21.ap-northeast-1.compute.internal>
2023-01-22 10:11:51 +0000

    Task4

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,7 +7,7 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
       if @order.save
         flash[:notice] = 'ModelClassName was successfully created.'
-        redirect_to "/orders"
+        redirect_to "/orders/confirm"
       else
         render :new
       end
@@ -22,7 +22,16 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
+    @cart_items = current_customer.cart_items
+    @order.shipping_cost = 800
+    #binding.pry
   end
+
 
   def complete
   end
