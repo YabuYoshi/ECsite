55dbe16ddefd1f747c06b280cce64c6a0ad7a42a
EC2 Default User <ec2-user@ip-172-31-33-21.ap-northeast-1.compute.internal>
2023-01-23 14:45:15 +0000

    Task4

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details ,dependent: :destroy
   enum payment_method: { credit_card: 0, transfer: 1}
+  enum order_status: { wait: 0, confirm: 1, making: 2, ready: 3,　sent:4 }
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,5 @@
 class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
+  enum making_status: { impossible: 0, wait: 1, making: 2, complete:3 }
 end
